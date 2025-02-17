7b7bf7804cb7f29fa005931c1985bf850f29e0e3
Fayo08 <oyendefavour12@gmail.com>
2025-02-17 13:47:49 -0700

    sum of array using reduce method

--- simpleArraySum.js
+++ simpleArraySum.js
@@ -1,6 +1,14 @@
-ar = [1,2,3,4,10,11]
-    let sum = 0
-    for (let i = 0; i < ar.length; i++) {
-        sum += ar[i]
-    }
-    console.log(sum)
+ar = [1, 2, 3, 4, 10, 11];
+let sum = 0;
+for (let i = 0; i < ar.length; i++) {
+  sum += ar[i];
+}
+console.log(sum);
+
+function simpleArraySum(ar) {
+  ar = [1, 2, 3, 4, 10, 11];
+  const sum = ar.reduce((total, amount) => total + amount);
+  return sum;
+}
+
+console.log(simpleArraySum());
